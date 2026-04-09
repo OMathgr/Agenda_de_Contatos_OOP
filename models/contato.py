70ecf81decd87d3a517fd8ae94fc48d7e5b3a771
OMathgr <def.facer@outlook.com>
2026-04-09 00:10:03 -0300

    feat: Adiciona sistema de validação nas entradas do contato

--- models/contato.py
+++ models/contato.py
@@ -1,3 +1,5 @@
+import re
+
 class Contato:
     def __init__(self, nome, telefone, email):
         self.nome = nome
@@ -8,7 +10,38 @@ class Contato:
         return f"{self.nome} | {self.telefone} | {self.email}"
     
     def __repr__(self):
-        return f"Contato('{self.nome})', '{self.telefone}', '{self.email}')"
+        return f"Contato('{self.nome}', '{self.telefone}', '{self.email}')"
     
     def __eq__(self, outro):
         return self.email.lower() == outro.email.lower()
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, valor):
+        if not valor.strip():
+            raise ValueError("Nome não pode ser vazio!")
+        self.__nome = valor
+
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @telefone.setter
+    def telefone(self, valor):
+        limpo = re.sub(r'\D', '', valor)
+        if len(limpo) < 10 or len(limpo) > 11:
+            raise ValueError("Telefone Inválido!")
+        self.__telefone = limpo
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, valor):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", valor):
+            raise ValueError("Email inválido!")
+        self.__email = valor
